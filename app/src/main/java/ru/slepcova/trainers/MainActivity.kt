9408675bd26c02f0498961e9265a63445bb1dbe5
radioperez <spaghetticious@gmail.com>
2025-03-08 13:14:43 +0700

    Добавлены основы StartScreen

--- app/src/main/java/ru/slepcova/trainers/MainActivity.kt
+++ app/src/main/java/ru/slepcova/trainers/MainActivity.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import ru.slepcova.trainers.ui.theme.TrainersTheme
@@ -20,8 +26,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TrainersTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    StartScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +36,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun StartScreen(modifier: Modifier = Modifier) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+           verticalArrangement = Arrangement.SpaceEvenly) {
+        Row(verticalAlignment = Alignment.Top,
+            horizontalArrangement = Arrangement.End) {
+            Text("settings", modifier = modifier)
+        }
+        Text("stats", modifier = modifier)
+        Text("history", modifier = modifier)
+        Sheet(modifier)
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun Sheet(modifier: Modifier = Modifier) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            text = "Начать свободную тренировку",
+            modifier = modifier
+        )
+        Text(
+            text = "Начать шаблон 1",
+            modifier = modifier
+        )
+        Text(
+            text = "Редактор шаблонов",
+            modifier = modifier
+        )
+    }
+}
+
+@Preview
+@Composable
+fun StartScreenPreview() {
     TrainersTheme {
-        Greeting("Android")
+        StartScreen()
     }
 }
